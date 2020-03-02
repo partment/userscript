eb01ae320d52e2c0f9407def6018736dc2526ee1
PartMent <partment@live.com>
2020-03-02 22:43:33 +0800

    Update Twitter No Ads to 1.2
    Fix crash twitter problem.

--- twitter.no.ads.user.js
+++ twitter.no.ads.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Twitter No Ads
-// @version 1.1
+// @version 1.2
 // @author PartMent
 // @description Automatically remove Twitter Ads.
 // @match https://mobile.twitter.com/
@@ -15,10 +15,10 @@
 let MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
 if (MutationObserver) console.log('No Ads is enabled.');
 let observer = new MutationObserver(e => {
-    let ads = document.querySelector('div div div article div div div div:nth-of-type(5):not([role="group"]) div span').parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+    let ads = document.querySelector('div div div article div div div div:nth-of-type(5):not([role="group"]) div span');
     if (ads) {
+        ads = ads.parentNode.parentNode.parentNode.parentNode.parentNode;
         ads.remove();
-        document.querySelector('div div div[data-testid="placementTracking"]').parentNode.parentNode.remove();
         console.log('Ads have been removed.');
     }
 });
