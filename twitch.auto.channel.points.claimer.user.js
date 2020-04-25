d1bc1f38965ed7caa137a35f25ff37188273396a
PartMent <partment@live.com>
2020-04-26 01:18:34 +0800

    Fix double click problem

--- twitch.auto.channel.points.claimer.user.js
+++ twitch.auto.channel.points.claimer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Twitch Auto Channel Points Claimer
-// @version 1.5.1
+// @version 1.6
 // @author PartMent
 // @description Automatically claim channel points.
 // @match https://www.twitch.tv/*
@@ -11,14 +11,18 @@
 // ==/UserScript==
 
 let MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+let claiming = false;
 if (MutationObserver) console.log('Auto claimer is enabled.');
 let observer = new MutationObserver(e => {
     let bonus = document.querySelector('.claimable-bonus__icon');
-    if (bonus) {
+    if (bonus && !claiming) {
         bonus.click();
+        let date = new Date();
+        claiming = true;
         setTimeout(() => {
-            console.log('Claimed!');
-        }, Math.random() * 1000 + 5000);
+            console.log('Claimed at '+date);
+            claiming = false;
+        }, Math.random() * 1000 + 2000);
     }
 });
 observer.observe(document.body, {childList: true, subtree: true});
